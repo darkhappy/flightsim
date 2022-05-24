d4bb92a479d035a3998304bf8ed7ded1a913460d
darkhappy <darkhappyy@gmail.com>
2022-05-24 16:33:07 -0400

    Generator: Added comments

--- Generator/Models/AirplaneFactory.cs
+++ Generator/Models/AirplaneFactory.cs
@@ -2,16 +2,39 @@ using System;
 
 namespace Generator.Models
 {
+  /// <summary>
+  ///   Factory for creating <see cref="Airplane" /> objects.
+  /// </summary>
   public class AirplaneFactory
   {
+    /// <summary>
+    ///   The instance of the <see cref="AirplaneFactory" />
+    /// </summary>
     private static AirplaneFactory? _instance;
 
+    /// <summary>
+    ///   Constructor for the <see cref="AirplaneFactory" />.
+    ///   <remarks>
+    ///     It is private to prevent instantiation, and to ensure that the <see cref="AirplaneFactory" /> is a singleton.
+    ///   </remarks>
+    /// </summary>
     private AirplaneFactory()
     {
     }
 
+    /// <summary>
+    ///   Gets the instance of the <see cref="AirplaneFactory" />. If the instance does not exist, it will be created.
+    /// </summary>
     public static AirplaneFactory Instance => _instance ??= new AirplaneFactory();
 
+    /// <summary>
+    ///   Creates a new <see cref="Airplane" />.
+    /// </summary>
+    /// <param name="data">The information required to create the Airplane.</param>
+    /// <returns>
+    ///   <see cref="Airplane" />
+    /// </returns>
+    /// <exception cref="NotSupportedException">Thrown if the <see cref="AirplaneType" /> is invalid.</exception>
     public Airplane CreateAirplane(AirplaneInfo data)
     {
       var type = data.Type;
--- Generator/Models/AirplaneType.cs
+++ Generator/Models/AirplaneType.cs
@@ -1,5 +1,8 @@
 namespace Generator.Models
 {
+  /// <summary>
+  ///   Represents the type of <see cref="Airplane" />.
+  /// </summary>
   public enum AirplaneType
   {
     Fight,
