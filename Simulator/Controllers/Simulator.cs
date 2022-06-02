c35759fea1e818057b8315e0f7dc591e0ab8739c
answr42Hub <78607026+answr42Hub@users.noreply.github.com>
2022-06-02 15:04:12 -0400

    added comments

--- Simulator/Controllers/Simulator.cs
+++ Simulator/Controllers/Simulator.cs
@@ -15,26 +15,48 @@ namespace Simulator.Controllers
     private static Simulator _instance;
     private readonly FormSimulator _frmSim;
     private Scenario _scenario;
+
+    /// <summary>
+    /// Constructo of a singleton instance of Simulator
+    /// </summary>
     private Simulator()
     {
       _frmSim = new FormSimulator();
-      Initialize(_frmSim.Path());
+      //Initialize(_frmSim.Path());
       Application.Run(_frmSim);
     }
 
+    /// <summary>
+    /// Checks if there is alredy an existing instance of Simulator, if not, it creates one
+    /// </summary>
     public static Simulator Instance => _instance ?? (_instance = new Simulator());
 
+    /// <summary>
+    /// Entry point of the Simulator program
+    /// </summary>
+    /// <param name="args"></param>
     [STAThread]
     public static void Main(string[] args)
     {
       new Simulator();
     }
+
+    /// <summary>
+    /// This metyhode intializes the simulation before the first Tick.
+    /// It imports the selected scenario and places all elements in the right place
+    /// </summary>
+    /// <param name="path">
+    /// This is the path to the serialized .xml scenario file.
+    /// </param>
     public void Initialize(string path)
     {
       Import(path);
       SetAirportPositions();
     }
 
+    /// <summary>
+    /// Takes all airports from the scenario airports list and places them on the map.
+    /// </summary>
     public void SetAirportPositions()
     {
       foreach (var airport in _scenario.Airports)
@@ -44,6 +66,10 @@ namespace Simulator.Controllers
       }
     }
 
+    /// <summary>
+    /// If the airplanes are moving on the map, it places them a the right places. 
+    /// </summary>
+    /// <param name="airport"></param>
     public void SetAirplanesPositions(Airport airport)
     {
       foreach (var airplane in airport.Airplanes)
@@ -60,6 +86,12 @@ namespace Simulator.Controllers
       }
     }
 
+    /// <summary>
+    /// It makes alle the actions to be made in a given time from the scenario.
+    /// </summary>
+    /// <param name="time">
+    /// Time given in wich the scenario gets updated
+    /// </param>
     public void OnTick(double time)
     {
       /*
@@ -72,11 +104,21 @@ namespace Simulator.Controllers
       _scenario.HandleTick(time);
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <exception cref="NotImplementedException"></exception>
     private void UpdateEvents()
     {
       throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Method that takes a .xml file and transforms it into a scenario.
+    /// </summary>
+    /// <param name="path">
+    /// This is the path to the serialized .xml scenario file.
+    /// </param>
     public void Import(string path)
     {
       var serializer = new DataContractSerializer(typeof(Scenario));
